bcb0963fad45793400bbc08134d27221b83e0c8a
Ravern Koh <ravernkoh@gmail.com>
2024-10-08 10:00:07 +0800

    add links to resources

--- generate.sh
+++ generate.sh
@@ -3,3 +3,4 @@ curl -s http://127.0.0.1:3999/static/slides.js | sed -E 's/(\/static\/)/.\1/' > 
 curl -s http://127.0.0.1:3999/static/styles.css > docs/static/styles.css
 curl -s http://127.0.0.1:3999/static/notes.css > docs/static/notes.css
 curl -s http://127.0.0.1:3999/introduction-to-go.slide | sed -E 's/(\/static\/)/.\1/' | sed -E '/play\.js/d' > docs/index.html
+cp -r images docs/images
